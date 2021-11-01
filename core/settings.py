32044bad2ce18f7f17d40bc575a38c7c8c8bf041
jorgegso <hajorge2012@gmail.com>
2021-11-01 02:18:41 -0300

    add

--- core/settings.py
+++ core/settings.py
@@ -32,7 +32,6 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 
-# podemos trabajar con cualquier host '*'
 ALLOWED_HOSTS = ['*']
 
 
