f07de2a382292ddc62a18ca006e3ecd8a12b0fd0
jorgegso <hajorge2012@gmail.com>
2021-11-01 02:21:28 -0300

    add

--- core/settings.py
+++ core/settings.py
@@ -19,7 +19,6 @@ env = environ.Env()
 environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-#donde se encuentra nuestro directorio django
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
